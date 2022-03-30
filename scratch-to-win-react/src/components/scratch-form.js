c9801b0a1494aeb42d67cf49dc75b4b6e3f4b348
Jef Ogues <jef.hotmail_@hotmail.com>
2022-03-30 19:19:48 -0400

    Created Form Page and css file

---
+++ scratch-to-win-react/src/components/scratch-form.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+import Form from "./form";
+import '../css/game-stlye.css';
+import prizeDetail from '../images/prize_detail.png'
+
+class ScratchForm extends Component {
+    render() {
+        return(
+            <div className="game-container">
+                <Form/>
+                <div className="prize-container">
+                <img className="prize-details" src={prizeDetail} />
+                </div>
+            </div>
+        )
+    }
+}   
+
+export default ScratchForm;
