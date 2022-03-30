f1e11bb34d008f7ef2550fd32fd7ea6fb7a9da1f
Jef Ogues <jef.hotmail_@hotmail.com>
2022-03-30 19:21:12 -0400

    For Testing Purposes Commented out "ScratchHome" to Test  "ScratchForm"

--- scratch-to-win-react/src/App.js
+++ scratch-to-win-react/src/App.js
@@ -4,12 +4,14 @@ import './css/style.css';
 import Header from './components/header.js';
 import Footer from './components/footer.js';
 import ScratchHome from './components/scratch-home.js';
+import ScratchForm from './components/scratch-form';
 
 function App() {
   return (
     <div className='app'>
       <Header />
-      <ScratchHome />
+      {/* <ScratchHome /> */}
+      <ScratchForm />
       <Footer />
     </div>
   );
