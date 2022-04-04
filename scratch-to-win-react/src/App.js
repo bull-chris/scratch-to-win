af00486a2bd1963d03ac5414145a6251546dab47
kakdiyaharshil <78425725+kakdiyaharshil@users.noreply.github.com>
2022-04-04 12:01:23 -0500

    Revert "Revert "legal page created and connected with form""
    This reverts commit f7c1cc15b0e75f76730eb4617c57b097acb0af19.

--- scratch-to-win-react/src/App.js
+++ scratch-to-win-react/src/App.js
@@ -10,7 +10,7 @@ import Footer from './components/footer.js';
 import ScratchHome from './components/scratch-home.js';
 import ScratchForm from './components/scratch-form';
 import ScratchGame from './components/scratch-game';
-
+import ScratchLegal from'./components/legal-page';
 function App() {
   return (
     <BrowserRouter>
@@ -20,7 +20,7 @@ function App() {
         <Route path="/" element={<ScratchHome />} />
         <Route path="/form" element={<ScratchForm />} />
         <Route path="/game" element={<ScratchGame />} />
-        {/* <Route path="/legal" element={<ScratchLegal />} /> */}
+        <Route path="/legal" element={<ScratchLegal />} /> 
       </Routes>
       
       <Footer />
